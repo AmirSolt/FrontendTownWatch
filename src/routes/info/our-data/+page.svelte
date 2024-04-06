3fb3af3d94ed018dedce12d8fc9337eecc128d09
Amir <soltamir194@gmail.com>
2024-04-06 12:28:09 -0400

    upd

--- src/routes/info/our-data/+page.svelte
+++ src/routes/info/our-data/+page.svelte
@@ -1,4 +1,17 @@
-<p>
-	We process the data published by the local police department. We do our best to weed out incorrect
-	or incomplete reports for better accuracy.
-</p>
+<h1 class="text-3xl font-bold">Our Data</h1>
+
+<div class="p-4">
+	<p>
+		We start by gathering information from two main sources: the Ontario Provincial Police and other
+		authorized sources via their public APIs. After collecting the data, we carefully go through
+		each piece to make sure it's accurate and complete. We check for any mistakes or missing details
+		and fix them as needed. This helps us ensure that the information we provide on our platform is
+		reliable and trustworthy for our users.
+	</p>
+	<p>
+		After obtaining the data, we collect the user's location and periodically scan our database for
+		recent police reports, typically on an hourly basis. If any police reports are detected near the
+		user's location, we promptly notify them. This proactive approach ensures that users stay
+		informed about any relevant incidents occurring in their vicinity in a timely manner.
+	</p>
+</div>
