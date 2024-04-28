423947a4cb20ffb8350b506f8edf61ea89698063
Amir <soltamir194@gmail.com>
2024-04-28 09:24:33 -0400

    upd

--- src/routes/(pages)/donate/success/+page.svelte
+++ src/routes/(pages)/donate/success/+page.svelte
@@ -11,8 +11,8 @@
 <div class="card-mod flex flex-col justify-center items-center gap-4">
 	<Smile color="#15CE48" size={150} />
 
-	<div class="flex flex-col justify-center items-center gap-2">
-		<h1 class="text-4xl">Thank you for your donation!</h1>
+	<div class="flex flex-col justify-center items-center text-center gap-2">
+		<h1 class="text-xl font-bold">Thank you for your donation!</h1>
 		<p>Your help means a lot.</p>
 	</div>
 
